550c839c61ba7fa4051074e924011eea7b7969cf
DvorahC <51906224+DvorahC@users.noreply.github.com>
2019-09-20 11:12:16 +0300

    Update numpy8.py

--- numpy8.py
+++ numpy8.py
@@ -4,23 +4,21 @@ Write a function that accepts a NumPy array that represents an X-Circle board
 and returns True if there is a winner in the game. 
  '''
 
-
 def is_there_a_winner(a):
     good_line = np.array(['X', 'X', 'X'])
     rows = a[0:, :]
-    diagonales = np.diag(a)
     columns = a[:, 0:]
-    if rows == good_line or diagonales == good_line or columns == good_line:
+    if (rows == good_line).all(axis=1).any() or(a.diagonal() == good_line).all() or (columns == good_line).all(axis=0).any():
         return True
     else:
         return False
 
 
 a = np.array([
-  ['X', '', 'X'],
-  ['', ' ', 'O'],
-  ['X', ' ', ' ']
+  ['X', ' X', ' '],
+  [' ', '', 'O'],
+  ['X', ' ', 'X']
 ])
 
-
 print(is_there_a_winner(a))
+
